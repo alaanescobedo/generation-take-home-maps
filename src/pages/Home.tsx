ff7b2eb3d9b8ef1f1fe2749f6e5b6596151ef28d
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-24 21:40:56 -0600

    fix: Fav data doesn't load when the app starts (#1)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
 import { MapStores, FavoriteStoresContainer } from "../containers";
 import { PillGithub } from "../components/Pill/PillGithub";
 import styles from "./Home.module.css";
+import { useEffect } from "react";
+import { createStore } from "../models";
+import { IDataStore } from "../types";
+
+import data from '../store_directory.json'
+import { useMapStores } from "../store/Maps.context";
 
 export const Home = () => {
+
+  const { setAllStores, favoriteStores } = useMapStores()
+
+  useEffect(() => {
+    const dataMarkers = data.map((dataStore: IDataStore, i) => {
+      const myStore = createStore(i, dataStore);
+
+      favoriteStores.forEach((favStore) => {
+        if (myStore.id === favStore.id) myStore.isFavorite = true
+      })
+      return myStore
+    });
+
+    setAllStores(dataMarkers)
+  }, [])
+
   return (
     <div className={`App ${styles.container}`}>
       <MapStores />
