09be430e5afb59b646556019d655ddce3346bde4
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-24 21:17:04 -0600

    fix: Fav data doesn't load when the app starts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import Home from "./pages/Home";
 import "./App.css";
 import { MapStoresProvider } from "./store/Maps.provider";
-import data from "./store_directory.json";
-import { createStore } from "./models";
-import { IDataStore } from "./types";
 
-const dataMarkers = data.map((store: IDataStore, i) => {
-  return createStore(i, store);
-});
 function App() {
 
   return (
     <>
-      <MapStoresProvider stores={dataMarkers}  >
+      <MapStoresProvider>
         <Home />
       </MapStoresProvider>
     </>
