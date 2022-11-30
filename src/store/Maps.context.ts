c5bb18ba3e0b95c59ca65812684fbe4b9c314cd4
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-30 08:37:39 -0600

    feat: Stores.context - remove implementation from MapStores.context

--- src/store/Maps.context.ts
+++ src/store/Maps.context.ts
@@ -1,27 +1,18 @@
 import { createContext, useContext, Dispatch, SetStateAction } from "react";
-import { IStore } from "../types";
 
-interface MapStoreContext {
-  activeStore: IStore | null
-  setActiveStore: Dispatch<SetStateAction<IStore | null>>
-  allStores: IStore[]
-  setAllStores: Dispatch<SetStateAction<IStore[]>>
-  favoriteStores: IStore[]
-  setFavoriteStores: (store: IStore[]) => void
+interface IMapContext {
   zoom: number
   setZoom: Dispatch<SetStateAction<number>>
   center: google.maps.LatLngLiteral
   setCenter: Dispatch<SetStateAction<google.maps.LatLngLiteral>>
-  removeFavorite: (store: IStore) => void
-  addFavorite: (store: IStore) => void
 }
 
-export const MapStoresContext = createContext<MapStoreContext | null>(null)
+export const MapContext = createContext<IMapContext | null>(null)
 
-export const useMapStores = () => {
-  const context = useContext(MapStoresContext)
+export const useMap = () => {
+  const context = useContext(MapContext)
   if (!context) {
-    throw new Error("Cannot use 'useMapStores' outside of 'MapStoresProvider'")
+    throw new Error("Cannot use 'useMap' outside of 'MapStoresProvider'")
   }
   return context
 }
